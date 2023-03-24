ea1983e71a999dde73dd83f8b962134509386941
Ivan <ipkozyrin@gmail.com>
2023-03-24 20:46:34 +0300

    add

---
+++ lib/MemStorage.ts
@@ -0,0 +1,55 @@
+import IndexedEvents from './IndexedEvents.js';
+
+
+type ChangeEventHandler = (dir: string, name: string, value: any, oldValue: any) => void
+
+
+/**
+ * It is storage in memory.
+ * It means that all the data will be lost on system stop.
+ */
+export class MemStorage {
+  readonly changeEvent = new IndexedEvents<ChangeEventHandler>()
+
+  private readonly storage: Record<string, any> = {}
+
+
+  async get(dir: string, name: string): Promise<any> {
+    return this.storage[dir][name]
+  }
+
+  async getNames(dir: string): Promise<string[]> {
+    return Object.keys(this.storage[dir])
+  }
+
+  async set(dir: string, name: string, value: any) {
+    if (!this.storage[dir]) this.storage[dir] = {}
+
+    const oldValue = this.storage[dir][name]
+
+    this.storage[dir][name] = value
+
+    this.changeEvent.emit(dir, name, value, oldValue)
+  }
+
+  async remove(dir: string, name: string) {
+    if (!this.storage[dir]) return
+
+    const oldValue = this.storage[dir][name]
+
+    delete this.storage[dir][name]
+
+    this.changeEvent.emit(dir, name, undefined, oldValue)
+  }
+
+  async clear(dir: string) {
+    const oldDir = this.storage[dir]
+
+    delete this.storage[dir]
+
+    for (const name of Object.keys(oldDir)) {
+      this.changeEvent.emit(dir, name, undefined, oldDir[name])
+    }
+  }
+
+}
