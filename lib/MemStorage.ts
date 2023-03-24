bd9432c8cb210a7b2392ff6ba198c579658afb95
Ivan <ipkozyrin@gmail.com>
2023-03-24 20:56:50 +0300

    add

--- lib/MemStorage.ts
+++ lib/MemStorage.ts
@@ -3,12 +3,22 @@ import IndexedEvents from './IndexedEvents.js';
 
 type ChangeEventHandler = (dir: string, name: string, value: any, oldValue: any) => void
 
+export interface MemStorageBase {
+  readonly changeEvent: IndexedEvents<ChangeEventHandler>
+  initStorage(storage: Record<string, Record<string, any>>): Promise<void>
+  get(dir: string, name: string): Promise<any>
+  getNames(dir: string): Promise<string[]>
+  set(dir: string, name: string, value: any): Promise<void>
+  remove(dir: string, name: string): Promise<void>
+  clear(dir: string): Promise<void>
+}
+
 
 /**
  * It is storage in memory.
  * It means that all the data will be lost on system stop.
  */
-export class MemStorage {
+export class MemStorage implements MemStorageBase {
   readonly changeEvent = new IndexedEvents<ChangeEventHandler>()
 
   private storage: Record<string, Record<string, any>> = {}
