1d11e956600ab1de1898506eee7f73c9dd9d0315
Ivan <ipkozyrin@gmail.com>
2023-04-25 22:05:21 +0300

    add

--- lib/mkdirPLogic.ts
+++ lib/mkdirPLogic.ts
@@ -1,8 +1,8 @@
-import {pathDirname, pathBasename, pathIsAbsolute, PATH_SEP} from './paths.js';
-import {trimCharEnd} from './strings.js';
+import {pathDirname, pathBasename, pathIsAbsolute, PATH_SEP} from './paths.js'
+import {trimCharEnd} from './strings.js'
 
 
-export default async function mkdirPLogic (
+export async function mkdirPLogic (
   pathToDir: string,
   isDirExists: (dirName: string) => Promise<boolean>,
   mkdir: (dirName: string) => Promise<void>
@@ -10,53 +10,51 @@ export default async function mkdirPLogic (
   if (!pathIsAbsolute(pathToDir)) {
     throw new Error(`path "${pathToDir}" has to be absolute`);
   }
+  else if (pathToDir.indexOf('~') === 0) {
+    throw new Error(`The ~ as root path doesn't supported`)
+  }
+  // if dir exists do nothing
+  if (await isDirExists(pathToDir)) return false;
 
-  if (isDirExists(pathToDir)) return false;
+  const preparedPath = trimCharEnd(pathToDir, PATH_SEP)
+  // not existent dirs from closest to further
+  const dirsToCreate: string[] = []
+  // dir which is already exists
+  let existentBasePath: string = PATH_SEP
 
-  const preparedPath = trimCharEnd(pathToDir, PATH_SEP);
-
-  // path parts from closest to further
-  const pathParts: string[] = [];
-  let existentBasePath: string = '/';
-
-  function recursionFind(localPathToDir: string) {
-    // TODO: why ~ - only absolute paths are supported
-    //if (!localPathToDir || localPathToDir === PATH_SEP || localPathToDir === '~' || localPathToDir === `~${PATH_SEP}`) {
-    // skip root path
+  async function recursionFind(localPathToDir: string) {
     if (!localPathToDir || localPathToDir === PATH_SEP) {
-      return;
+      return
     }
-    else if (isDirExists(localPathToDir)) {
-      existentBasePath = localPathToDir;
-
+    else if (await isDirExists(localPathToDir)) {
+      // save existent part of path
+      existentBasePath = localPathToDir
       // finish of finding
-      return;
+      return
     }
     else {
+      // Dir doesn't exist at the moment
       // split path
-      const shorterPath = pathDirname(localPathToDir);
-      const lastPart = pathBasename(localPathToDir);
-
-      pathParts.push(lastPart);
-
+      const shorterPath = pathDirname(localPathToDir)
+      const lastPart = pathBasename(localPathToDir)
+      // store it to be further created
+      dirsToCreate.push(lastPart)
       // go deeper
-      recursionFind(shorterPath);
+      await recursionFind(shorterPath)
     }
   }
 
-  recursionFind(preparedPath);
+  await recursionFind(preparedPath);
 
-  // TODO: why return false??
-  //if (!existentBasePath) return false;
+  if (!dirsToCreate.length) return false
 
   // create paths
-  for (let pathIndex in pathParts.reverse()) {
-    const pathPart = pathParts.slice(0, parseInt(pathIndex) + 1)
-      .join(PATH_SEP);
+  for (let pathIndex in dirsToCreate.reverse()) {
+    const pathPart = dirsToCreate.slice(0, parseInt(pathIndex) + 1).join(PATH_SEP)
     const fullPath = `${existentBasePath}${PATH_SEP}${pathPart}`;
 
-    mkdir(fullPath);
+    await mkdir(fullPath)
   }
 
-  return true;
+  return true
 }
