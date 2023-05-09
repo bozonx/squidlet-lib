72b94cf51f43682a946d3324a5be3bce2736a8dd
Ivan <ipkozyrin@gmail.com>
2023-05-09 15:50:13 -0300

    version 0.1.3

---
+++ distr/interfaces/AsyncLogger.d.ts
@@ -0,0 +1,10 @@
+export interface AsyncLogger {
+    debug: (msg: string) => Promise<void>;
+    info: (msg: string) => Promise<void>;
+    warn: (msg: string) => Promise<void>;
+    error: (msg: string) => Promise<void>;
+    /**
+     * Means emit in any case not depends on log level
+     */
+    log: (msg: string) => Promise<void>;
+}
