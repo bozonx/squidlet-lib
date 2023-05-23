1e1d6a78ea87e8503d399395864968f999415b92
Ivan <ipkozyrin@gmail.com>
2023-05-23 15:47:57 -0300

    add

---
+++ test/lib/deepManipulate.spec.js
@@ -0,0 +1,18 @@
+import {
+  deepGet,
+} from '../../lib/deepManipulate.js'
+
+
+describe('lib/deepManipulate', () => {
+
+  it('mergeDeepObjects', () => {
+    assert.equal(deepGet({a1: {b1: 1}}, 'a1.b1'), 1)
+    assert.deepEqual(deepGet({a1: {b1: 1}}, 'a1'), {b1: 1})
+    assert.equal(deepGet({a1: {b1: ['c1']}}, 'a1.b1[0]'), 'c1')
+    // not found
+    assert.isUndefined(deepGet({a1: {b1: 1}}, 'a1.b2'))
+    assert.isUndefined(deepGet({a1: {b1: ['c1']}}, 'a1.b1[2]'))
+    assert.isUndefined(deepGet({a1: {b1: 1}}, 'a2'))
+  })
+
+})
