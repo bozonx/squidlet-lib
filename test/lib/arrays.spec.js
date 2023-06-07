e0506f6dd06d99cd57fe2488d3449c27a6a25bc1
Ivan <ipkozyrin@gmail.com>
2023-06-07 12:52:52 -0300

    test

--- test/lib/arrays.spec.js
+++ test/lib/arrays.spec.js
@@ -5,6 +5,14 @@ describe('lib/arrays', () => {
   it('deduplicate', () => {
     assert.deepEqual(deduplicate(['a', 'b', 'a', 'a']), ['a', 'b'])
     assert.deepEqual(deduplicate([1, 2, 1, 1]), [1, 2])
+    assert.deepEqual(deduplicate([undefined, 2, 1, 1]), [undefined, 2, 1])
+    assert.deepEqual(deduplicate([undefined, undefined, 1]), [undefined, 1])
+    assert.deepEqual(deduplicate([null, null, 1]), [null, 1])
+    assert.deepEqual(deduplicate([false, false, 1]), [false, 1])
+    assert.deepEqual(deduplicate([0, 0, 1]), [0, 1])
+    assert.deepEqual(deduplicate(['', '', 1]), ['', 1])
+
+    // TODO: test arrays and objects
   })
 
 })
